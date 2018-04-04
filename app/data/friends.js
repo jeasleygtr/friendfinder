245b96e605ef6223c0aad2dbcb70019ca5a0f35d
unknown <jeasleygtr@icloud.com>
2018-04-04 11:53:04 -0400

    added friend options

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,7 +15,143 @@ var friendArray = [
             '1'
         ],
         aboutme: 'Cowabunga dude!!'
-    }
+    },
+    {
+        name: 'Donatello',
+        photo: 'https://static.comicvine.com/uploads/square_small/4/49448/5635228-donatello+2.jpg',
+        scores: [
+            '2',
+            '2',
+            '2',
+            '2',
+            '2',
+            '2',
+            '2',
+            '2',
+            '2',
+            '2'
+        ],
+        aboutme: 'I can build anything!!'
+    },
+    {
+        name: 'Raphael',
+        photo: 'https://aa1a5178aef33568e9c4-a77ea51e8d8892c1eb8348eb6b3663f6.ssl.cf5.rackcdn.com/p/full/1ade153a-8e3a-4294-b889-87bb07dc7cac.jpg',
+        scores: [
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3'
+        ],
+        aboutme: 'Seriously, you can do better than that!!'
+    },
+    {
+        name: 'Leonardo',
+        photo: 'https://static.comicvine.com/uploads/original/4/49448/5635214-leonardo.jpg',
+        scores: [
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4'
+        ],
+        aboutme: 'A true ninja is a master of himself and his environment!!'
+    },
+    {
+        name: 'Splinter',
+        photo: 'https://i.pinimg.com/originals/de/ba/ff/debaff702df4201d23a6065c0019f188.png',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ],
+        aboutme: 'The true ninja is wise!!'
+    },
+    {
+        name: 'Casey jones',
+        photo: 'https://i0.wp.com/www.teenagemutantninjaturtles.com/wp-content/uploads/2014/03/Casey-Jones-Teenage-Mutant-Ninja-Turtles-1.jpg',
+        scores: [
+            '1',
+            '5',
+            '1',
+            '5',
+            '1',
+            '5',
+            '1',
+            '5',
+            '1',
+            '5'
+        ],
+        aboutme: 'Wanna play cricket??'
+    },
+    {
+        name: 'Shredder',
+        photo: 'https://vignette.wikia.nocookie.net/tmnt/images/e/e9/Shredder87.png/revision/latest?cb=20130919210006',
+        scores: [
+            '2',
+            '3',
+            '5',
+            '2',
+            '1',
+            '2',
+            '4',
+            '5',
+            '1',
+            '4'
+        ],
+        aboutme: 'I will get those turtles!!'
+    },
+    {
+        name: 'April ONeil',
+        photo: 'https://i.ytimg.com/vi/5Aqobsq83yg/maxresdefault.jpg',
+        scores: [
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '2'
+        ],
+        aboutme: 'Where is the camera?  This has to be on the news!!'
+    },
+    {
+        name: 'Keno',
+        photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTljRdD5hZNy346b-mrEJtyvhVXNM6sTt3LQaGjWVgGOnrpA9-pHQ',
+        scores: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '5',
+            '4',
+            '3',
+            '2',
+            '1'
+        ],
+        aboutme: 'I can fight!!'
+    },
 ];
 
 module.exports = friendArray;
